16ce70bf56bddf7e50c3212092d301699f6a60de
Ricardo Zambon <ricardo.zambon@hotmail.com>
2020-10-24 21:33:28 -0300

    Styled tables with bootstrap

--- SmartSchool/src/app/alunos/alunos.component.ts
+++ SmartSchool/src/app/alunos/alunos.component.ts
@@ -10,13 +10,13 @@ export class AlunosComponent implements OnInit {
   public titulo = 'Alunos';
 
   public alunos = [    
-    { nome: 'Marta' },
-    { nome: 'Paula' },
-    { nome: 'Laura' },
-    { nome: 'Luiza' },
-    { nome: 'Lucas' },
-    { nome: 'Pedro' },
-    { nome: 'Paulo' }
+    { id: 1, nome: 'Marta', sobrenome: 'Kent', telefone: '33225555' },
+    { id: 2, nome: 'Paula', sobrenome: 'Isabela', telefone: '3354288' },
+    { id: 3, nome: 'Laura', sobrenome: 'Antonia', telefone: '55668899' },
+    { id: 4, nome: 'Luiza', sobrenome: 'Maria', telefone: '6565659' },
+    { id: 5, nome: 'Lucas', sobrenome: 'Machado', telefone: '565685415' },
+    { id: 6, nome: 'Pedro', sobrenome: 'Alvares', telefone: '466454545' },
+    { id: 7, nome: 'Paulo', sobrenome: 'José', telefone: '9874512' }
   ];
 
   constructor() { }
